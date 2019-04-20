e1ac9414dd8c91950bba8d5d6dfdf5cab91ce19b
Amy Cen <amy.cen@blackrock.com>
2019-04-20 04:17:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,4 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   organized_data = {}
@@ -6,6 +7,7 @@ def nyc_pigeon_organizer(data)
       organized_data[name] ||= {}
       organized_data[name][category] ||= []
       organized_data[name][category] << value
+      binding.pry
     end
   end
   organized_data
