c24af3eb37824243e7e030ef4b0c27e3de9eebf8
Amy Cen <amy.cen@blackrock.com>
2019-04-20 04:14:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   organized_data = {}
   data.each do |category, attrib|
-
+    attrib.each do |value, name|
+      organized_data[name] ||= {}
+      organized_data[name][category] ||= []
+      organized_data[name][category] << value
+      
 end
